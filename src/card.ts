320a5a92b06309ca1a20ac044de05c41abb2a2ae
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-03-15 14:37:10 +0700

    Init

--- src/card.ts
+++ src/card.ts
@@ -20,7 +20,8 @@ export default class Card {
 
     private initPkcs11(): void {
         try {
-            Card.pkcs11.load("C:\\Windows\\System32\\beidpkcs11.dll");
+            // Card.pkcs11.load("C:\\Windows\\System32\\beidpkcs11.dll");
+            Card.pkcs11.load('./beidpkcs11.dll');
             Card.pkcs11.C_Initialize();
         } catch (error) {
             console.error(error);
